cf1a9dc506349f8eae94c9b623b0f596213ee64a
nastyamariyevskaya <123410948+nastyamariyevskaya@users.noreply.github.com>
2023-01-27 01:11:45 +0000

    обновила list elements

--- src/Layouts/Page/AboutCompany/Components/Addresses/ListElement.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/ListElement.js
@@ -6,18 +6,14 @@ import IconOnlyButton from '../../../../../Components/IconOnlyButton.js';
 
 const Option = styled.div`
     outline: none;
-    border-radius: 0px;
-    border: solid #152842 1px;
-    background-color: #D9E2EF;
-    color: #666787;
 `;
 
 function ListElement(props) {
     return (
         <div className='d-flex flex-row align-items-center align-self-stretch gap-2'>
             <Option className='flex-grow-1 p-2'>{props.children}</Option>
-            <EditIcon/>
-            <RemoveIcon/>
+            <IconOnlyButton icon={<EditIcon/>}/>
+            <IconOnlyButton icon={<RemoveIcon/>}/>
         </div>
     );
 }
